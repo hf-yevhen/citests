7f7cd5c4bf1d18f7e7e881a10e4545aa28dcf6a5
evhen.lahodyk <evhen.lahodyk@hotline.finance>
2021-08-16 16:23:12 +0300

    Init commit

---
+++ App.js
@@ -0,0 +1,63 @@
+import React, {useState} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+  Pressable,
+} from 'react-native';
+
+const App = () => {
+  const [isShow, setIsShow] = useState(true);
+
+  const onPressFunction = () => setIsShow(show => !show);
+
+  return (
+    <SafeAreaView style={styles.backgroundStyle}>
+      <StatusBar barStyle="dark-content" />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={styles.backgroundStyle}>
+        <View testID="Text_Container" style={styles.textContainer}>
+          <Text testID="Test_String">Test string</Text>
+        </View>
+        <View style={styles.divider} />
+        <View testID="Test_Button_Container" style={styles.pressableContainer}>
+          <Pressable testID="Toggle_Button" onPress={onPressFunction}>
+            <Text>Press to toggle</Text>
+          </Pressable>
+          <View style={styles.divider} />
+
+          {isShow && <Text testID="Disappearing_Text">This isn't hidden</Text>}
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  backgroundStyle: {
+    backgroundColor: '#fff',
+  },
+  textContainer: {
+    height: 50,
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#99d5ff',
+  },
+  pressableContainer: {
+    height: 100,
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#aff2fa',
+  },
+  divider: {
+    height: 20,
+  },
+});
+
+export default App;
---
+++ e2e/firstTest.e2e.js
@@ -0,0 +1,33 @@
+describe('Example CI Tests', () => {
+  beforeAll(async () => {
+    await device.launchApp();
+  });
+
+  // beforeEach(async () => {
+  //   await device.reloadReactNative();
+  // });
+
+  it('should have all Views', async () => {
+    await expect(element(by.id('Text_Container'))).toBeVisible();
+    await expect(element(by.id('Test_Button_Container'))).toBeVisible();
+  });
+
+  it('should have all Texts', async () => {
+    await expect(element(by.id('Test_String'))).toBeVisible();
+    await expect(element(by.id('Disappearing_Text'))).toBeVisible();
+  });
+
+  it('should hide text after tap', async () => {
+    await element(by.id('Toggle_Button')).tap();
+    await expect(element(by.id('Disappearing_Text'))).toBeNotVisible();
+  });
+
+  it('should show text after tap', async () => {
+    await element(by.id('Toggle_Button')).tap();
+    await expect(element(by.id('Disappearing_Text'))).toBeVisible();
+  });
+
+  it('should fail tests', async () => {
+    await element(by.id('NOT_EXISTING_ELEMENT')).tap();
+  });
+});
